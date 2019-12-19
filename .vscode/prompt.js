1d97e3c63b19668a9a9396688950ed6ff08740b8
Ashley Casimir <ashcas@me.com>
2019-12-19 10:01:57 -0800

    js code

---
+++ .vscode/prompt.js
@@ -0,0 +1,10 @@
+var computer;
+computer = prompt("Are you looking for a Mac or PC?")
+
+if(computer == "mac"){
+    alert("Check out our used Macbook Pros!")
+}else if(computer == "pc"){
+    alert("Our used custom computers are our most popular!")
+}else{
+    alert("Dont know? Feel free to browse")
+}
