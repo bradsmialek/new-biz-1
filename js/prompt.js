4a28ea1ab0983df52598ff47ef9b75a859deb0a5
Ashley Casimir <ashcas@me.com>
2019-12-20 09:55:35 -0800

    new function

--- js/prompt.js
+++ js/prompt.js
@@ -1,12 +1,35 @@
 var computer;
-computer = prompt("Are you looking for a Mac or PC?")
+var order;
+    
+var welcomePrompt = function(){
 
-if(computer == "mac"){
-    alert("Check out our used Macbook Pros!")
-}else if(computer == "pc"){
-    alert("Our used custom computers are our most popular!")
-}else{
-    alert("Dont know? Feel free to browse")
+    computer = prompt("Are you looking for a Mac or PC?");
+
+    while(computer !== "mac" && computer !== "pc"){
+        
+        computer = prompt("Please enter mac or pc...");
+    }
+    if(computer == "mac" || computer == "pc"){
+        order = prompt("How many would you like?");
+    }
+        
 }
+        
 
-document.write()
+    
+
+//     if(computer == "mac"){
+//         alert("Check out our used Macbook Pros!")
+//     }else if(computer == "pc"){
+//         alert("Our used custom computers are our most popular!")
+//     }else{
+//         alert("Dont know? Feel free to browse")
+//     }
+// }
+
+// var showOrder = function(){
+//     var order = prompt("how many do you need?")
+ 
+//     while(order)
+// }
+welcomePrompt()
